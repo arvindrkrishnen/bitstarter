0a9ec677d3acbad0f0eb0d536ddb4351f90b2626
arvindrkrishnen <arvind.radhakrishnen@gmail.com>
2013-07-21 20:19:26 +0000

    updates on web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-var app = express();
+var app = express.createServer();
+var fs = require('fs');
+
 app.use(express.logger());
 
+var array = fs.readFileSync('index.html').toString().split("\n");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+for (i in array)
+{
+  response.send(array[i]);
+}
 });
 
 var port = process.env.PORT || 5000;
