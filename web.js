1b561458275610c0f1b2d2d24c32f22465ee8881
arvindrkrishnen <arvind.radhakrishnen@gmail.com>
2013-07-22 06:39:36 +0000

    new web & index

--- web.js
+++ web.js
@@ -1,20 +1,49 @@
 var express = require('express');
 var app = express.createServer();
-var fs = require('fs');
 
-app.use(express.logger());
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
 
-var array = fs.readFileSync('index.html').toString().split("\n");
+http.createServer(function(request, response) {
 
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
 
-app.get('/', function(request, response) {
-for (i in array)
-{
-  response.send(array[i]);
-}
-});
+  var contentTypesByExtension = {
+    '.html': "text/html",
+    '.css':  "text/css",
+    '.js':   "text/javascript"
+  };
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var headers = {};
+      var contentType = contentTypesByExtension[path.extname(filename)];
+      if (contentType) headers["Content-Type"] = contentType;
+      response.writeHead(200, headers);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
