6285901ef0fc5c8541b9fae1eb1eaee35a028b28
arvindrkrishnen <arvind.radhakrishnen@gmail.com>
2013-07-28 18:05:35 +0000

    added one space

--- web.js
+++ web.js
@@ -5,5 +5,6 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {    
    var html = fs.readFileSync(htmlfile).toString();    
    response.send(html);});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {  console.log("Listening on " + port);});
